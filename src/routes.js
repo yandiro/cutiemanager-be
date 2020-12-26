e750adef96fd61142e1ef3ed33f8ca682d1b531b
Tulio Yandiro Sandoval <tuliobbs@gmail.com>
2020-12-26 19:16:45 +1100

    routes using external files

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const routes = express.Router();
 
+const CutieController = require('./controllers/CutieController')
+const UserController = require('./controllers/UserController')
+
 routes.get('/', (req, res) => {
     return res.json({ hELLO: "oi" })
 })
 
-routes.post('/cuties', (req, res) => {
-    return res.json({ ok: true })
-})
+routes.post('/cuties', CutieController.store);
+routes.post('/users', UserController.store);
 
 module.exports = routes;
 
