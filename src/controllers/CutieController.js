9b2caf9460b4a5cd2483b2f7bee9eee9bb1aadc4
Tulio Yandiro Sandoval <tuliobbs@gmail.com>
2020-12-28 14:31:05 +1100

    exporting controllers after function is defined, not together

--- src/controllers/CutieController.js
+++ src/controllers/CutieController.js
@@ -1,36 +1,36 @@
 const Cutie = require('../models/Cutie');
 const User = require('../models/User');
 
-module.exports = {
-    async store(req, res) {
+async function store(req, res) {
 
-        const { name } = req.body;
-        const { user: userId } = req.headers;
+    const { name } = req.body;
+    const { user: userId } = req.headers;
 
-        let message;
+    let message;
 
-        await Cutie.create({
-            name,
-            position: 0,
-            'owned-by': userId
-        },
-            async (err, instance) => {
-                if (err) {
-                    console.log(err);
+    await Cutie.create({
+        name,
+        position: 0,
+        'owned-by': userId
+    },
+        async (err, instance) => {
+            if (err) {
+                console.log(err);
 
-                    res.statusCode = 500;
-                    message = 'error on craeting registry';
-                } else {
-                    res.statusCode = 201;
-                    message = 'created';
-                }
-
-                const user = await User.findById(userId);
-                user.cuties.push(instance._id);
-                await user.save();
-
-                return res.json({ message: message, idCreated: instance._id });
+                res.statusCode = 500;
+                message = 'error on craeting registry';
+            } else {
+                res.statusCode = 201;
+                message = 'created';
             }
-        )
-    }
+
+            const user = await User.findById(userId);
+            user.cuties.push(instance._id);
+            await user.save();
+
+            return res.json({ message: message, idCreated: instance._id });
+        }
+    )
 }
+
+module.exports = { store }
